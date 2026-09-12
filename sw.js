0463fddb6eed1418ebfb0acca5eed29b3e20c87b
JeanBallanFilho <jeanballan@gmail.com>
2026-09-12 15:00:54 -0300

    Otimizar horizontalmente e diminuir espaco entre linhas da Lista de Check-ins do Salao

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker para GRUPO QU4TRO CRM - PWA Offline-First */
-const CACHE_NAME = 'qu4tro-crm-v1.0.13';
+const CACHE_NAME = 'qu4tro-crm-v1.0.14';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
